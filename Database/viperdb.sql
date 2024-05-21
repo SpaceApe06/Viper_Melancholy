3784d4862e9fa4c2b76ebca682e5034a8d914850
SpaceApe <144213974+SpaceApe06@users.noreply.github.com>
2024-05-21 09:56:49 +0200

    updated database

--- Database/viperdb.sql
+++ Database/viperdb.sql
@@ -26,12 +26,11 @@ CREATE TABLE `stats` (
   `stat_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `click` int(11) NOT NULL,
-  `V_shard` int(11) NOT NULL,
   `kills` int(11) NOT NULL,
   PRIMARY KEY (`stat_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `stats_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +39,31 @@ CREATE TABLE `stats` (
 
 LOCK TABLES `stats` WRITE;
 /*!40000 ALTER TABLE `stats` DISABLE KEYS */;
+INSERT INTO `stats` VALUES
+(1,NULL,11,0),
+(2,NULL,11,0),
+(3,NULL,11,0),
+(4,NULL,11,0),
+(5,NULL,11,0),
+(6,NULL,11,0),
+(7,NULL,11,0),
+(8,NULL,11,0),
+(9,NULL,11,0),
+(10,NULL,11,0),
+(11,NULL,11,0),
+(12,NULL,11,0),
+(13,NULL,13,0),
+(14,NULL,22,0),
+(15,NULL,60,1),
+(16,NULL,60,1),
+(17,3,41,0),
+(18,3,41,0),
+(19,3,41,0),
+(20,3,41,0),
+(21,3,41,0),
+(22,3,41,0),
+(23,4,41,0),
+(24,5,0,0);
 /*!40000 ALTER TABLE `stats` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,10 +76,12 @@ DROP TABLE IF EXISTS `upgrades`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `upgrades` (
   `upgrade_id` int(11) NOT NULL AUTO_INCREMENT,
-  `upgrade_name` varchar(25) DEFAULT NULL,
-  `upgrade_description` varchar(255) DEFAULT NULL,
-  `bought` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`upgrade_id`)
+  `user_id` int(11) DEFAULT NULL,
+  `upgrade_double` tinyint(1) DEFAULT NULL,
+  `upgrade_venom` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`upgrade_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `upgrades_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -81,7 +107,7 @@ CREATE TABLE `users` (
   `password` varchar(255) DEFAULT NULL,
   `admin` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,10 +117,10 @@ CREATE TABLE `users` (
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES
-(1,'test1','1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',NULL),
 (2,'test1','1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',NULL),
 (3,'aaa','9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0',NULL),
-(4,'Admin','c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f',1);
+(4,'Admin','c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f',1),
+(5,'spsis','0278e0499baae60795387ddf071a660d4a50928ccbc455b33b54cb1b3c5311f7',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-16 12:08:10
+-- Dump completed on 2024-05-21  9:57:32
