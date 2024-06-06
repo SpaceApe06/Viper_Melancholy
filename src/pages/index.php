e5714dc20e2f79931d75811834f9d22df18c9737
Deadguana <alexandermofre@gmail.com>
2024-06-06 12:08:43 +0200

    Fikset litt css

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,30 +6,36 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 	<body>
+        <div id=logg_inn_body>
+            
+            <div id=Logg_inn_logo_container>
+                <img src="public/Viper_melancholy_logo.svg" alt="Viper Melancholy logo" id="Logg_inn_logo" height="100px">
+                <p id="Logo_tekst"> Vipers's Melancholy is a clicker game where you click to upgrade the 
+                                  snake against enemies and bosses. </p>
+            </div>
 
-        <br><br>
-        <h1>Viper's Melancholy</h1>
-		<p>
-			Vipers's Melancholy is a clicker game where you click to upgrade the
-			snake against enemies and bosses.
-		</p>
-        <!-- id="loggInn" -->
-        <form method="post" action="log_inn.php">
-                <div id="logg_inn_label">
-                    <label for="username">Name:</label>
-                    <input type="text" id="username" name="username" placeholder="Username" maxlength="20">
-                </div>
-                <div id="logg_inn_label">
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password" maxlength="20">
-                </div>
-                <!-- hvis brukeren eksisterer går den til påmelding siden, hvis ikke er det en link til å registrere seg -->
 
+
+            <!-- logg inn -->
+            <div id="logg_inn">
+                <form method="post" action="log_inn.php">
+                    <div id="logg_inn_label">
+                        <label for="username">Name:</label>
+                        <input type="text" id="username" name="username" placeholder="Username" maxlength="20">
+                    </div>
+                    <div id="logg_inn_label">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Password" maxlength="20">
+                    </div>
+
+                    <!-- hvis brukeren eksisterer går den til påmelding siden, hvis ikke er det en link til å registrere seg -->
                     <div id="login_button_container">
                         <button id="Login_button" type="submit">Login</button><br/>
                     </div>
-
+                        
                     <p>Don't have an account? <a href="register_page.php">Sign Up</a></p>
-        </form>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
